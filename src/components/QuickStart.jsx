baccd86c928a849f2d17348c924451d9b21df82a
LazarBrusin <ninaasda882@gmail>
2024-11-08 05:13:27 +0100

    initial commit

---
+++ src/components/QuickStart.jsx
@@ -0,0 +1,48 @@
+import { Card, Timeline, Typography } from "antd";
+import React, { useMemo } from "react";
+import { useMoralis } from "react-moralis";
+
+const { Text } = Typography;
+
+const styles = {
+  title: {
+    fontSize: "20px",
+    fontWeight: "700",
+  },
+  text: {
+    fontSize: "16px",
+  },
+  card: {
+    boxShadow: "0 0.5rem 1.2rem rgb(189 197 209 / 20%)",
+    border: "1px solid #e7eaf3",
+    borderRadius: "0.5rem",
+    width: "100%"
+  },
+  timeline: {
+    marginBottom: "-45px",
+  },
+};
+
+export default function QuickStart({ isServerInfo }) {
+  const { Moralis } = useMoralis();
+
+  const isInchDex = useMemo(
+    () => (Moralis.Plugins?.oneInch ? true : false),
+    [Moralis.Plugins?.oneInch],
+  );
+
+  return (
+    <div style={{ display: "flex", gap: "10px", width: "500px", height: "500px" }}>
+      <Card
+        style={styles.card}
+        title={
+          <>
+            📝 <Text strong>Dashboard</Text>
+          </>
+        }
+      >
+
+      </Card>
+    </div>
+  );
+}
